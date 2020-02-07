90efcdbd1b822752a0eda035688bc4c90cbfdb40
Dinglebopper <dinglebopper@dales.us>
2020-02-07 13:02:07 -0600

    Adding unit test sandbox

---
+++ UnitTest/SenderTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TestApp;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class SenderTest
+    {
+        [Test]
+        public void EmailSenderSuccess()
+        {
+        }
+
+        [Test]
+        public void EmailSenderHaxor()
+        {
+        }
+
+        [Test]
+        public void EmailSenderBomb()
+        {
+        }
+    }
+}
